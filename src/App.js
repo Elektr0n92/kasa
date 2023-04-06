542c16e60abd051be4c12d609431db0017a09614
Timothé Lauvernier <timothe@lauvernier.fr>
2023-04-06 18:04:14 +0200

    Setting up json file in app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import data from "./datas/houseList.json";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  data.map((obj) => console.log(obj.description));
+  return <h1>bonjour</h1>;
 }
 
 export default App;
