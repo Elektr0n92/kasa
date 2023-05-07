92444bf45b77debb6d417c2c7d2ea67dddb222e5
Timothé <109433280+Elektr0n92@users.noreply.github.com>
2023-05-07 05:28:57 +0200

    add responsive error/About/Home page
    * fix home direction error page with env gh-pages
    
    * add responsive home page
    
    * add fix
    
    * fix collaspse responsive
    
    * fix change img banner for mobile device
    
    * apply responsive about page
    
    * error responsive and ajust height page-container

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./styles/style.css";
 
 function App() {
+  const isGitHubPages = process.env.PUBLIC_URL !== "";
   return (
     <div className="wrapper">
       <Router>
@@ -25,7 +26,10 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/404" element={<Error />} />
             <Route path="/products/:productId" element={<Product />} />
-            <Route path="*" element={<Navigate to="/404" replace />} />
+            {isGitHubPages && (
+              <Route path="*" element={<Navigate to="/" replace />} />
+            )}
+            {!isGitHubPages && <Route path="*" element={<Error />} />}
           </Routes>
           <Footer />
         </div>
